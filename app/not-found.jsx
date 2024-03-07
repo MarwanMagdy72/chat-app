de46e2c7244c806ecbf082fb52d3bd9db3a0bc93
MarwanMagdy72 <115204176+MarwanMagdy72@users.noreply.github.com>
2024-03-07 19:37:25 +0200

    add not-found page

---
+++ app/not-found.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function NotFound() {
+  return (
+    <>
+       <div  className='bg-gray-300 flex justify-center items-center h-screen w-full flex-col' >
+            <h1> 404 | Not Found Page !  </h1>
+
+       </div>
+    </>
+  )
+}
+
+export default NotFound
